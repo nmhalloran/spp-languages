1a29bc72f57bab512290ed3870d02345ac30cafe
Nicholas Halloran <nmhalloran@hotmail.com>
2018-01-07 17:54:03 +0000

    developing function for user to learn vocab. Only admin ca add vocab.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,14 @@
 class WordsController < ApplicationController
+  get "words/:slug/learn_word" do
+    if is_logged_in?
+      @user = Speaker.find(session[:user_id])
+      @language = Language.find_by_slug(params[:slug])
+      erb :'/words/add_word'
+    else
+      redirect to '/'
+    end
+
+  end
 
 
 end
